b7c11fc546036d82406548e2121e46d692c31aca
melkemk <melkemk503@gmail.com>
2024-08-01 11:31:29 +0300

    style

--- task_4/src/App.tsx
+++ task_4/src/App.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect, Key } from 'react';
 import './App.css';
-
+import Task from './components/Task';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 function App() {
   const [value, setValue] = useState('');
   const [tasks, setTasks] = useState(() => {
@@ -11,11 +14,14 @@ function App() {
     return [];
   });
 
+
+  
   useEffect(() => {
     localStorage.setItem('tasks', JSON.stringify(tasks));
   }, [tasks]);
 
   function deleteItem(valu:string) {
+
     const updatedTasks = tasks.filter((val: string) => val !== valu);
     setTasks(updatedTasks);
   }
@@ -24,6 +30,20 @@ function App() {
     const updatedTasks = tasks.map((val:string) => (val !== oldVal ? val : newVal));
     setTasks(updatedTasks);
   }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function handleKeyDown(e: { key: string }) {
+    if (e.key === 'Enter') {
+      handleClick();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [value]);
 
   function handleClick() {
     if (tasks.includes(value) || value === '') {
@@ -35,32 +55,6 @@ function App() {
     setValue('');
   }
 
-  function Task({ val }: { val: string }) {
-    const [x, setX] = useState(val);
-    return (
-      <div className="add">
-        <input
-          type="text"
-          value={x}
-          onChange={(e) => setX(e.target.value)}
-        />
-        <button
-          id={val}
-          className="delete"
-          onClick={() => deleteItem(val)}
-        >
-          Delete
-        </button>
-        <button
-          id={val}
-          className="update"
-          onClick={() => updateItem(val, x)}
-        >
-          Update
-        </button>
-      </div>
-    );
-  }
 
   return (
     <>
@@ -72,15 +66,16 @@ function App() {
             placeholder="task"
             name="task"
             value={value}
+            onKeyDown={handleKeyDown}
             onChange={(e) => setValue(e.target.value)}
           />
-          <button id="add" onClick={handleClick}>
-            add
+          <button id="addd" onClick={handleClick}>
+          <FontAwesomeIcon icon={faPlus as IconProp} style={{ color: 'green' }} />
           </button>
         </div>
         <div id="tasks">
           {tasks.map((task: string, index: Key | null | undefined) => (
-            <Task val={task} key={index} />
+            <Task val={task} key={index} updateItem={updateItem} deleteItem={deleteItem} />
           ))}
         </div>
       </div>
---
+++ task_4/src/components/Task.tsx
@@ -0,0 +1,48 @@
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { useState } from "react";
+import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
+
+
+interface TaskProps {
+  val: string;
+  updateItem: (oldVal: string, newVal: string) => void;
+  deleteItem: (val: string) => void;
+}
+
+const Task = ({ val, updateItem, deleteItem }:TaskProps)  =>
+   {  const [x, setX] = useState(val);
+    const [initial ,setinitial] = useState(val);
+    function setx(){
+      setinitial(x)
+      updateItem(val, x)
+    }
+    return (
+      <div className="add">
+        <input
+          type="text"
+          value={x}
+          onChange={(e) => {setX(e.target.value)}
+
+        }
+        />
+       
+        <button
+          id={val}
+          onClick={() => setx()}
+        >
+          <FontAwesomeIcon icon={(initial===x?faEdit:faCheck  )as IconProp } style={{ color: 'blue' }} />
+        </button>
+        <button
+          id={val}
+          onClick={() => deleteItem(val)}
+        >
+          <FontAwesomeIcon icon={faTrash as IconProp} style={{ color: 'red' }} />
+        </button>
+      </div>
+    );
+  }
+
+  export default Task;
